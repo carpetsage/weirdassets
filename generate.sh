bed067ec98f734d73284f8032bc62698989e7f3f
fanaticscripter <fanaticscripter@gmail.com>
2021-02-16 00:33:52 +0800

    Add egginc-extras/icon_afx_stone_slot.png
    I had to draw this myself in Affinity Designer.

--- generate.sh
+++ generate.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env zsh
+force=0
+[[ $1 == -f || $1 == --force ]] && force=1
 for size in 32 64 128 256; do
     mkdir -p $size/egginc $size/egginc-extras
 done
 for src in orig/egginc/*.png orig/egginc-extras/*.png; do
     for size in 32 64 128 256; do
         dst=${size}/${src#orig/}
+        (( !force )) && [[ -e $dst ]] && continue
         echo "$src => $dst"
         convert $src -resize ${size}x${size} $dst
         optipng -quiet $dst
