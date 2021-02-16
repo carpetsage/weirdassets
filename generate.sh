bf20dc20849dcce08e459a202e944e243caa9585
fanaticscripter <fanaticscripter@gmail.com>
2021-02-16 12:46:35 +0800

    Add artifact rarity glow effect backgrounds provided by @mikit

--- generate.sh
+++ generate.sh
@@ -2,9 +2,9 @@
 force=0
 [[ $1 == -f || $1 == --force ]] && force=1
 for size in 32 64 128 256; do
-    mkdir -p $size/egginc $size/egginc-extras
+    mkdir -p $size/egginc $size/egginc-extras $size/egginc-extras/glow
 done
-for src in orig/egginc/*.png orig/egginc-extras/*.png; do
+for src in orig/egginc/*.png orig/egginc-extras/**/*.png; do
     for size in 32 64 128 256; do
         dst=${size}/${src#orig/}
         (( !force )) && [[ -e $dst ]] && continue
