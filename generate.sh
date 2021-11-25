e7ef1241a10ad6319ab86894bfdaf03d3d80dc45
fanaticscripter <fanaticscripter@gmail.com>
2021-11-25 11:18:39 +0800

    Add webp version of images

--- generate.sh
+++ generate.sh
@@ -5,11 +5,22 @@ for size in 32 64 128 256; do
     mkdir -p $size/egginc $size/egginc-extras $size/egginc-extras/glow
 done
 for src in orig/egginc/*.png orig/egginc-extras/**/*.png; do
+    dst=$src:r.webp
+    ((( force )) || [[ ! -e $dst ]]) && {
+        echo "$src => $dst"
+        convert $src -define webp:lossless=true $dst
+    }
     for size in 32 64 128 256; do
         dst=${size}/${src#orig/}
-        (( !force )) && [[ -e $dst ]] && continue
-        echo "$src => $dst"
-        convert $src -resize ${size}x${size} $dst
-        optipng -quiet $dst
+        ((( force )) || [[ ! -e $dst ]]) && {
+            echo "$src => $dst"
+            convert $src -resize ${size}x${size} $dst
+            optipng -quiet $dst
+        }
+        dst=$dst:r.webp
+        ((( force )) || [[ ! -e $dst ]]) && {
+            echo "$src => $dst"
+            convert $src -resize ${size}x${size} $dst
+        }
     done
 done
